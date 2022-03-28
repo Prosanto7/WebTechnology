51d28f32407b26a0cf85679ff772ee489b77a19a
Prosanto7 <prosantodeb7@gmail.com>
2022-03-28 12:42:45 +0600

    Sum Sub Mul Div

--- HTML FILES/cal.js
+++ HTML FILES/cal.js
@@ -1,5 +1,33 @@
-function showName()
+function calculate()
 {
-    var sh=4;
-    document.getElementById("show").value=sh;
+    var num1,num2;
+    num1 = parseInt(document.getElementById("first").value);
+    num2 = parseInt(document.getElementById("second").value);
+
+    var sum = num1 + num2;
+    var sub = num1 - num2;
+    var mul = num1 * num2;
+    var div = num1 / num2;
+
+    document.getElementById("sum").value = sum;
+    document.getElementById("sub").value = sub;
+    document.getElementById("mul").value = mul;
+    document.getElementById("div").value = div;
+}
+
+function getGreater()
+{
+    var num1,num2;
+
+    num1 = parseInt(document.getElementById("first").value);
+    num2 = parseInt(document.getElementById("second").value);
+
+    if(num1>num2)
+    {
+        document.getElementById("greater").value = num1;
+    }
+    else
+    {
+        document.getElementById("greater").value = num2;
+    }
 }
