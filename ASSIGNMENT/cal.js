9a27d02a81def8aebdd7b014d7d12ce28ba5bc86
Prosantodeb7 <prosantodeb7@gmail.com>
2022-03-29 07:11:40 +0600

    Array Badic Concept Cleared

--- ASSIGNMENT/cal.js
+++ ASSIGNMENT/cal.js
@@ -1,4 +1,21 @@
 function show()
 {
-    prompt();
+    showAgain();
+}
+
+function showAgain()
+{
+    const args = [1,2,3,4,5];
+    args.push(100);
+
+    window.alert(args)
+
+    var output="";
+
+    for(let i=0;i<args.length;i++)
+    {
+        output = output + args[i] + "<br>"
+    }
+
+    document.getElementById("demo").innerHTML = output;
 }
