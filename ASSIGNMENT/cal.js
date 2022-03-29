ea13e6e3014d31a0cd932cf8fd19e74ad6bd9980
Prosantodeb7 <prosantodeb7@gmail.com>
2022-03-29 07:45:38 +0600

    Problem Solved

--- ASSIGNMENT/cal.js
+++ ASSIGNMENT/cal.js
@@ -10,11 +10,71 @@ function printArray(args)
     return output;
 }
 
+function getDifferenceArray(args)
+{
+    const differenceArray = [];
+
+    for(let i=0;i<args.length-1;i++)
+    {
+        differenceArray.push(Math.abs(args[i]-args[i+1]));
+    }
+
+    return differenceArray;
+}
+
+function getSequenceArray(n)
+{
+    const sequenceArray = [];
+
+    for(let i=1;i<=n-1;i++)
+    {
+        sequenceArray.push(i);
+    }
+
+    return sequenceArray;
+}
+
+function checkSequenceVsDifferenceArray(sequenceArray,differenceArray)
+{
+    let found=false;
+
+    for(let i=0;i<sequenceArray.length;i++)
+    {
+        for(let j=0;j<differenceArray.length;j++)
+        {
+            if(sequenceArray[i]==differenceArray[j])
+            {
+                found = true;
+            }
+        }
+        if(!found)
+        {
+            found = false;
+            break;
+        }
+    }
+
+    if(found)
+    {
+        return "Jolly";
+    }
+    else
+    {
+        return "Not Jolly";
+    }
+}
+
 function solveProblem()
 {
-    const args = [1,2,3,4,5];
-    args.push(100);
+    const args = [1,4,2,-1,6];
+
+    document.getElementById("inputString").innerHTML = printArray(args);
+
+    document.getElementById("differenceString").innerHTML = printArray(getDifferenceArray(args));
+
+    document.getElementById("sequneceString").innerHTML = printArray(getSequenceArray(args.length));
+
+
+    document.getElementById("outputString").innerHTML = checkSequenceVsDifferenceArray(getSequenceArray(args.length),getDifferenceArray(args));
     
-    window.alert(args);
-    document.getElementById("demo").innerHTML = printArray(args);
 }
